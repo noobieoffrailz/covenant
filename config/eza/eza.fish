763d8e982435c815d5ddc9b462f43f3058479081
Iivari Leinonen <iivari.leinonen@upcloud.com>
2026-09-25 18:46:32 +0300

    Drop unnecessary things

--- config/eza/eza.fish
+++ config/eza/eza.fish
@@ -1,4 +1,5 @@
 set -gx EZA_CONFIG_DIR "$HOME/.config/eza"
 
+alias l 'eza --long --icons --no-user -la'
 alias ls 'eza --long --icons --no-user -la'
 alias lt 'eza --tree --icons --no-user'
--- config/fish/conf.d/env.fish
+++ config/fish/conf.d/env.fish
@@ -1,2 +1,8 @@
 set -gx XDG_CONFIG_HOME $HOME/.config
-set -Ux HOMEBREW_PROFILE upbook
+
+switch (hostname -s)
+    case Upbook
+        set -gx HOMEBREW_PROFILE upbook
+    case Compooter
+        set -gx HOMEBREW_PROFILE compooter
+end
